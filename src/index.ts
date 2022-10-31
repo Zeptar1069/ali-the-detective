65428dd5a166ebb48d2b684674fe2b015fd9445c
Zeptar <terryk.onekorea@gmail.com>
2022-10-31 19:05:32 +0000

    job: test

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import 'dotenv/config';
+import express from 'express';
 import Client from './util/BaseClient';
+
+express().get('/', (_: any, res: any) => res.send('Online.')).listen(3000);
 
 new Client();
